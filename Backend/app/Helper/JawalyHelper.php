85bd56c08fa1296cbbcf246929d34accd4f5446b
Ahmed Nabil <mody998866@gmail.com>
2021-05-05 00:20:09 +0200

    Fix

--- Backend/app/Helper/JawalyHelper.php
+++ Backend/app/Helper/JawalyHelper.php
@@ -1,19 +1,31 @@
 <?php
 namespace App\Helper;
-use App\Models\Variable;
+header("Content-Type: text/html; charset=utf-8");
+
 use Http;
 class JawalyHelper{
 
 	public static function sendSMS($mobileNumber,$messageContent){
-		$response = Http::get('http://www.4jawaly.net/api/sendsms.php',[
-			'username' => 'servers',
-			'password' => 'aaa123',
-			'numbers' => $mobileNumber,
-			'message' => urlencode( $messageContent),
-			'sender' => 'DSC',
-			'unicode' => 'E',
-			'return' => 'json',
-		]);
-		return $response->json();
+		$text = urlencode( $messageContent);
+		$url = "http://www.4jawaly.net/api/sendsms.php?username=servers&password=aaa123&numbers=$mobileNumber&message=$text&sender=DSC&unicode=E&return=json";
+
+		$ret = file_get_contents($url);
+		// echo nl2br($ret);
+
+
+		// $header = array(
+		// 	"Content-Type" => 'text/html', 
+		// 	'charset'=>'utf-8'
+		// );  
+		// $response = Http::withHeaders($header)->get('http://www.4jawaly.net/api/sendsms.php',[
+		// 	'username' => 'servers',
+		// 	'password' => 'aaa123',
+		// 	'numbers' => $mobileNumber,
+		// 	'message' => urlencode( $messageContent),
+		// 	'sender' => 'DSC',
+		// 	'unicode' => 'E',
+		// 	'return' => 'json',
+		// ]);
+		// return $response->json();
 	}
 }
